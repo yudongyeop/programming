bcdd54c42f06cb418f89d1987ee415312656383a
YuDongYeop <48706998+yudongyeop@users.noreply.github.com>
2021-08-27 22:33:23 +0900

    change

--- 프로그래머스/K번째수.cpp
+++ 프로그래머스/K번째수.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     vector<int> tmp;
-    
+    //
     for (int i=0; i<commands.size(); i++) {
         for (int j= commands[i][0]-1; j < commands[i][1]; j++) {
             tmp.push_back(array[j]);
